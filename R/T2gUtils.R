fa46f7c9a732e1a4da6adf35a5317d36381eda55
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2018-07-19 14:42:25 -0500

    tweak the calcGeneCor method + switch API to expect samples by column and features by row, rather than vice versa + add options to specify the correlation method and the 'use' method + update documentation to reflect these changes and correct a typo + add sanity checks

--- R/T2gUtils.R
+++ R/T2gUtils.R
@@ -217,17 +217,24 @@ transToGeneCors <- function(data, t2g, numCores = 1L) {
 
 #' Calculate large gene expression correlation matrix in parallel
 #'
-#' This methods uses the ff package to calculate a large
-#' correlation matrix.
+#' This method uses the ff package to calculate a large
+#' correlation matrix in parallel.
 #'
 #' @param data a N x M matrix or data.frame, with N columns corresponding to
-#'   each gene observed, and M rows corresponding to the samples
-#'   in the experiment.
+#'   each samples in the experiment observed, and M rows corresponding to the
+#'   the genes observed.
 #' @param size the number of elements per block to split the data. The
 #'   default is 2000, which results in decent performance for typical
 #'   usage (order of 10-20K features). If the number of features is
 #'   not evenly divisible by the size parameter, a remainder block is used.
 #' @param numCores the number of cores to use for parallel calculations
+#' @param corMethod the method of calculating correlation. Supported
+#'   methods are 'pearson' (default), 'spearman', or 'kendall'. See
+#'   ?stats::cor for more details.
+#' @param useMethod what observations should be used? Supported options
+#'   are "everything" (default), "all.obs", "complete.obs", "na.or.complete",
+#'   or "pairwise.complete.obs". See ?stats::cor for more details on what
+#'   each option entails.
 #'
 #' @return an 'ff' object pointing to a file on disk containing the
 #'   gene correlation matrix
@@ -235,12 +242,30 @@ transToGeneCors <- function(data, t2g, numCores = 1L) {
 #' @importFrom parallel mclapply
 #' @importFrom stats cor
 #' @export
-calcGeneCor <- function(data, size = 2000, numCores = 1L) {
+calcGeneCor <- function(data, size = 2000, numCores = 1L,
+                        corMethod = "pearson", useMethod = "everything") {
   ## this code is adapted from two sources:
   ## 1) bigcor.R: http://www.dr-spiess.de/scripts/bigcor.R
   ## 2) bigcorPar.R: https://gist.github.com/bobthecat/5024079
-  data <- as.data.frame(data)
-  t_names <- colnames(data)
+  corMethod <- match.arg(corMethod, c('pearson', 'spearman', 'kendall'))
+  useMethod <- match.arg(useMethod, c('everything', 'all.obs', 'complete.obs',
+                                      'na.or.complete',
+                                      'pairwise.complete.obs'))
+  if (ncol(data) > nrow(data)) {
+    stop(paste0("There are more columns than row. Are you ",
+                "sure that each sample is a column, and each ",
+                "transcript is a row?"))
+  } else {
+    data <- as.data.frame(t(data))
+  }
+
+  g_names <- colnames(data)
+  if (is.null(t_names)) {
+    warning(paste0('There are no transcript IDs as column names. The ',
+                   'correlation matrix will have no IDs for row names or ',
+                   'column names.'))
+  }
+
   NCOL <- ncol(data)
   rest <- NCOL %% size
   large <- NCOL - rest
@@ -256,14 +281,13 @@ calcGeneCor <- function(data, size = 2000, numCores = 1L) {
   COMBS <- unique(COMBS)
 
   corMAT <- ff::ff(vmode = 'single', dim = c(NCOL, NCOL),
-                   dimnames = list(t_names, t_names))
-
+                   dimnames = list(g_names, g_names))
 
   result <- parallel::mclapply(1:nrow(COMBS), function(i) {
     comb <- COMBS[i, ]
     g1 <- SPLIT[[comb[1]]]
     g2 <- SPLIT[[comb[2]]]
-    COR <- stats::cor(data[, g1], data[, g2])
+    COR <- stats::cor(data[, g1], data[, g2], method = corMethod, use = useMethod)
     corMAT[g1, g2] <- COR
     corMAT[g2, g1] <- t(COR)
   }, mc.cores = numCores)
